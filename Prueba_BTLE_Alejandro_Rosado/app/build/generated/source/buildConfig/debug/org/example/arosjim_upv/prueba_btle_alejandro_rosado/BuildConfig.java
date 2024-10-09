a06c11ccaba692f12d0cc782305e3f7706cc2ac4
Alex <alexeltren@gmail.com>
2024-10-09 20:53:53 +0200

    Android v1.0 - Sprint 0

---
+++ Prueba_BTLE_Alejandro_Rosado/app/build/generated/source/buildConfig/debug/org/example/arosjim_upv/prueba_btle_alejandro_rosado/BuildConfig.java
@@ -0,0 +1,14 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.example.arosjim_upv.prueba_btle_alejandro_rosado;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "org.example.arosjim_upv.prueba_btle_alejandro_rosado";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Field from build type: debug
+  public static final String API_BASE_URL = "http://192.168.32.247:8080/api/v1/mediciones/";
+}
